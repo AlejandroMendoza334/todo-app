bdd9511c790b9625e1b74af54f735561716eebc1
AlejandroMendoza334 <alejandromendoza6575757@gmail.com>
2025-04-13 16:02:06 -0400

    To-Do App en React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [tarea, setTarea] = useState("");
+  const [lista, setLista] = useState([]);
+
+  const agregarTarea = () => {
+    if (tarea.trim() === "") return;
+    setLista([...lista, tarea]);
+    setTarea("");
+  };
+  const eliminarTarea = (index) => {
+    const nuevaLista = lista.filter((_, i) => i !== index);
+    setLista(nuevaLista);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>To-Do App</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Escribe una tarea"
+          value={tarea}
+          onChange={(e) => setTarea(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              agregarTarea();
+            }
+          }}
+        />
+        <button onClick={agregarTarea}>Agregar</button>
+      </div>
+
+      <ul>
+        {lista.map((t, i) => (
+          <li key={i}>
+            {t}
+            <button onClick={() => eliminarTarea(i)}>❌</button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
